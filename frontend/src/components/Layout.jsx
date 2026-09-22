9d71c06fd44896c81b4b8ee4be6d9ee4f647853e
Rajiv R. Yadav <139645725+Rjv-RY@users.noreply.github.com>
2026-09-22 06:35:57 +0530

    Added a little more color to the background and changed the light mode's white to cream

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-content-pattern">
       <Navbar />
       <main className="bg-content-bg max-w-[clamp(320px,80vw,1280px)] w-full mx-auto px-4 flex-grow">
         <Outlet />
